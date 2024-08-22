a7edf190f57a99969898b77b9709313b219789d9
GuilhermeViana14 <guilhermevianafilgueiras@gmail.com>
2024-08-22 17:36:41 -0300

    atualizado o stilo do app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,12 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
+      
       <Stack.Screen name="index"
-      options={{ headerTitle: '' }}
+     options={{ 
+      headerTitle: '',
+      headerStyle: { backgroundColor: '#87CEEB' }, // Cor do cabeçalho
+    }}
        />
     </Stack>
   );
